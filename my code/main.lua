35be6f30f6d082998b942480356505446ff6e72f
uarqt206 <lequangtrung100306@gmail.com>
2023-07-30 21:42:26 +0700

    pipe update

--- my code/main.lua
+++ my code/main.lua
@@ -1,6 +1,7 @@
 push = require 'libraries/push'
 Class = require 'libraries/class'
 require 'objects/Bird'
+require 'objects/Pipe'
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 VIRTUAL_WIDTH, VIRTUAL_HEIGHT = 512, 288
@@ -14,7 +15,10 @@ local GROUND_SPEED = 60
 local ground = love.graphics.newImage('pictures/ground.png')
 local groundScroll = 0
 
+local timeSpawner = 0
+
 bird = Bird()
+pipes = {}
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -24,8 +28,10 @@ function love.load()
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         resizable = true,
         fullscreen = false,
-        vsync = false
+        vsync = true
     })
+
+    math.randomseed(os.time())
 
     love.keyboard.keysPressed = {}
 end
@@ -51,12 +57,28 @@ function love.update(dt)
     groundScroll = (groundScroll + GROUND_SPEED * dt) % VIRTUAL_WIDTH
     bird:update(dt)
     love.keyboard.keysPressed = {}
+
+    timeSpawner = timeSpawner + dt
+    if timeSpawner > 2 then
+        table.insert(pipes, Pipe())
+        timeSpawner = 0
+    end
+
+    for k, pipe in pairs(pipes) do
+        pipe:update(dt)
+        if pipe.x < -PIPE_WIDTH then
+            table.remove(pipes, k) 
+        end
+    end
 end
 
 function love.draw()
     push:start()
 
     love.graphics.draw(background, -backgroundScroll, 0)
+    for k, pipe in pairs(pipes) do
+        pipe:render()
+    end
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
     bird:render()
     
