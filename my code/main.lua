14ceee89e1e3db0681fdbaca909ff2ee37f865ab
uraqt206 <lequangtrung100306@gmail.com>
2023-07-30 17:39:26 +0700

    new stuff

--- my code/main.lua
+++ my code/main.lua
@@ -1,5 +1,6 @@
 push = require 'libraries/push'
-class = require 'libraries/class'
+Class = require 'libraries/class'
+require 'objects/Bird'
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 VIRTUAL_WIDTH, VIRTUAL_HEIGHT = 512, 288
@@ -13,6 +14,8 @@ local GROUND_SPEED = 60
 local ground = love.graphics.newImage('pictures/ground.png')
 local groundScroll = 0
 
+bird = Bird()
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
@@ -23,15 +26,31 @@ function love.load()
         fullscreen = false,
         vsync = false
     })
+
+    love.keyboard.keysPressed = {}
 end
 
 function love.resize(w, h) 
     push:resize(w, h)
 end
 
+function love.keypressed(key) 
+    if key == 'escape' then
+        love.event.quit();
+    end
+
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key) 
+    return love.keyboard.keysPressed[key]
+end
+
 function love.update(dt) 
     backgroundScroll = (backgroundScroll + BACKGROUND_SPEED * dt) % BACKGROUND_LOOPING_POINT
     groundScroll = (groundScroll + GROUND_SPEED * dt) % VIRTUAL_WIDTH
+    bird:update(dt)
+    love.keyboard.keysPressed = {}
 end
 
 function love.draw()
@@ -39,6 +58,7 @@ function love.draw()
 
     love.graphics.draw(background, -backgroundScroll, 0)
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+    bird:render()
     
     push:finish()
 end
---
+++ my code/objects/Bird.lua
@@ -0,0 +1,23 @@
+Bird = Class{}
+GRAVITY = 20
+
+function Bird:init() 
+  self.image = love.graphics.newImage('pictures/bird.png')
+  self.height = self.image:getHeight()
+  self.width =  self.image:getWidth()
+  self.x = VIRTUAL_WIDTH/2 - self.width/2
+  self.y = VIRTUAL_HEIGHT/2 - self.height/2
+  self.dy = 0
+end
+
+function Bird:update(dt) 
+  if love.keyboard.wasPressed('space') then
+    self.dy = -5
+  end
+  self.dy = self.dy + GRAVITY * dt
+  self.y = self.y + self.dy
+end
+
+function Bird:render()
+  love.graphics.draw(self.image, self.x, self.y)
+end
